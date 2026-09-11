3e63d77c0284450ecbbc39696f19848b6e3e2fb9
sherly1998 <sherly_william@ymail.com>
2026-09-11 18:19:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,6 @@ window.NEXA_CONFIG = {
   supabaseUrl: 'https://rsuvnlrkgqvwqisyeoum.supabase.co',
   supabaseAnonKey: 'sb_publishable_mibhyNB9ROhMBY8Vhd6D3g_0_-U74OF'
 };
+const pbNexaPhase2 = document.createElement('script');
+pbNexaPhase2.src = 'phase-2.js';
+document.head.appendChild(pbNexaPhase2);
